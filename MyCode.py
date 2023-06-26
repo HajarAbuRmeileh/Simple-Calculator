9715677d3baf9896215320088a721933925a9942
HajarAbuRmeileh <hajar.maint@gmail.com>
2023-06-26 22:43:54 +0300

    SC-18

--- MyCode.py
+++ MyCode.py
@@ -3,6 +3,9 @@
 def add_numbers(a, b):
     return a + b
 
+def sub_numbers(a, b):
+    return a - b
+
 def get_user_input():
     a = float(input("first number:"))
     b = float(input("second number:"))
@@ -11,5 +14,7 @@ def get_user_input():
 if __name__ == "__main__":
     num1, num2 = get_user_input()
     add_result = add_numbers(num1, num2)
+    sub_result = sub_numbers(num1, num2)
     print("The sum of {} and {} is: {}".format(num1, num2, add_result))
+    print("The sum of {} and {} is: {}".format(num1, num2, sub_result))
 
